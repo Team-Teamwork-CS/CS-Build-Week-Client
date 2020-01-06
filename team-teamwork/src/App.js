b97884dea085203851f3612ff8c748b71474290a
Adam Hoffman <wintros@yahoo.com>
2020-01-06 14:54:16 -0700

    have room data coming back

--- team-teamwork/src/App.js
+++ team-teamwork/src/App.js
@@ -3,12 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import SignIn from "./Components/Auth/SignIn";
 import SignUp from "./Components/Auth/SignUp";
+import Home from "./Components/home/home.js"
 
 function App() {
   return (
     <div className="App">
       <SignIn />
       <SignUp />
+      <Home />
     </div>
   );
 }
