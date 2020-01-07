347f5698cf555f6db771ae3e080bd0f12758df82
Adam Hoffman <wintros@yahoo.com>
2020-01-07 15:08:11 -0700

    sprit

--- team-teamwork/src/features/map/index.js
+++ team-teamwork/src/features/map/index.js
@@ -1,16 +1,63 @@
 import React from "react";
+import { connect } from 'react-redux'
+import {SPRITE_SIZE} from '../../config/constants'
+import './styles.css'
+
+function getTileSprite(type) {
+  switch(type) {
+    case 0:
+      return "grass"
+    case 5: 
+      return "dirt"
+    case 6:
+      return "treasure"
+  }
+}
+
+function MapTile(props) {
+  return <div 
+  className={`tile ${getTileSprite(props.tile)}`}
+  style= {{
+    height: SPRITE_SIZE,
+    width: SPRITE_SIZE
+  }}
+>
+  
+</div>
+}
+
+function MapRow(props) {
+  return <div classname="row">
+    {
+      props.tiles.map(tile => <MapTile tile={tile} />)
+    }
+  </div>
+}
 
 function Map(props) {
   return (
     <div
       style={{
+        position: 'relative',
+        top: '0px',
+        left: "0px",
         width: "800px",
         height: "400px",
         backgroundColor: "green",
         border: "4px solid white"
       }}
-    ></div>
+    >
+      {
+        props.tiles.map(row => <MapRow tiles={row} />)        
+      }
+    </div>
   );
 }
 
-export default Map;
+function mapStateToProps(state) {
+  return {
+    tiles: state.map.tiles
+  }
+}
+
+export default connect(mapStateToProps)(Map);
--- team-teamwork/src/features/world/index.js
+++ team-teamwork/src/features/world/index.js
@@ -2,7 +2,13 @@ import React from "react";
 import Player from "../player";
 import Map from "../map/";
 
+import {tiles} from '../../data/map/1'
+import store from '../../config/store'
+
 function World(props) {
+  /*store.dispatch({type: 'ADD_TILES', payload: {
+    tiles
+  }})*/
   return (
     <div
       style={{
@@ -13,7 +19,7 @@ function World(props) {
       }}
     >
       <Player />
-      <Map />
+      <Map tiles={tiles} />
     </div>
   );
 }
